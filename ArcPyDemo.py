ed762786bbf373aeff09b876e4c24eeb589053d2
nhupp <nicolerhupp@gmail.com>
2020-05-29 15:40:39 -0700

    Added deadline for when I'm going to keep working on this...

--- ArcPyDemo.py
+++ ArcPyDemo.py
@@ -6,3 +6,4 @@ arcpy.CalculateField_management("c:/data/Portland.gdb/streets", "LENGTH_MILES", 
 arcpy.FeatureClassToFeatureClass_conversion("c:/data/Portland.gdb/streets", "Database Connections/MySDE.sde/PortlandDataset", "streets")
 
 #learned that I need to get python 2.7 i order to run ArcPy
+#going to try to DL the old python when I'm in the desert
