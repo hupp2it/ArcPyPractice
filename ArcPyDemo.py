e53f4c3540cf6c8065245cfe3ae7928fc4faab52
nhupp <nicolerhupp@gmail.com>
2020-05-29 15:35:10 -0700

    Added caveat that I need to get an old version of python in order to run ArcPy

--- ArcPyDemo.py
+++ ArcPyDemo.py
@@ -1,1 +1,8 @@
 print ("ArcPyDemo practice file")
+import arcpy
+
+arcpy.AddField_management("c:/data/Portland.gdb/streets", "LENGTH_MILES", "TEXT")
+arcpy.CalculateField_management("c:/data/Portland.gdb/streets", "LENGTH_MILES", "!shape.length@miles!", "PYTHON_9.3")
+arcpy.FeatureClassToFeatureClass_conversion("c:/data/Portland.gdb/streets", "Database Connections/MySDE.sde/PortlandDataset", "streets")
+
+#learned that I need to get python 2.7 i order to run ArcPy
